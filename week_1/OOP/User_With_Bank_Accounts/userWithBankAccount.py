032748f3d80177dd0b00de950cbc204ce543e51a
Joseph Brown <zdragoonmaster@aol.com>
2022-03-23 00:11:06 -0700

    updated bank account

--- week_1/OOP/User_With_Bank_Accounts/userWithBankAccount.py
+++ week_1/OOP/User_With_Bank_Accounts/userWithBankAccount.py
@@ -6,16 +6,22 @@ class BankAccount:
     def __repr__(self):
         return self.account
 
-class User(BankAccount):
-    def __init__(self, account_name, account, interest_rate, name):
-        super().__init__(account_name, account, interest_rate)
+class User:
+    def __init__(self, name, account_name, account, interest_rate, account_name2, account2, interest_rate2):
         self.name = name
-        # self.checking = BankAccount(100, .04)
+        self.savings = BankAccount(account_name, account, interest_rate)
+        self.checking = BankAccount(account_name2, account2, interest_rate2)
+
+# class User(BankAccount):
+#     def __init__(self, account_name, account, interest_rate, name):
+#         super().__init__(account_name, account, interest_rate)
+#         self.name = name
+#         # self.checking = BankAccount(100, .04)
     def __repr__(self):
         return self.name
 
-    def make_deposit(self, account):
-        self.account += account
+    def make_deposit(self, amount):
+        self.account += amount
         return self
 
     def make_withdrawl(self, amount):
@@ -26,11 +32,14 @@ class User(BankAccount):
         print(f"{self.name} {self.account_name} : {self.account}")
         return self
 
-anthony = User("checking", 100, .04, "anthony")
-anthony.make_deposit(100).make_withdrawl(50).display_user_balance()
-benjy = User("saving", 200, .08, "anthony")
-benjy.make_deposit(5000).make_withdrawl(2000).display_user_balance()
-chad = User("checking 2", 150, .09, "anthony")
-chad.make_deposit(10000).make_withdrawl(6000).display_user_balance()
-david = User("savings 2", 350, .09, "anthony")
-david.make_deposit(20000).make_withdrawl(7000).display_user_balance()
+anthony = User("anthony", "savings", 100, .05, "checking", 200, .01)
+anthony.make_deposit("savings", 300, .05)
+#anthony.savings.make_deposit()
+print(anthony.savings, anthony.checking)
+# anthony.make_deposit(100).make_withdrawl(50).display_user_balance()
+# benjy = User("saving", 200, .08, "anthony")
+# benjy.make_deposit(5000).make_withdrawl(2000).display_user_balance()
+# chad = User("checking 2", 150, .09, "anthony")
+# chad.make_deposit(10000).make_withdrawl(6000).display_user_balance()
+# david = User("savings 2", 350, .09, "anthony")
+# david.make_deposit(20000).make_withdrawl(7000).display_user_balance()
