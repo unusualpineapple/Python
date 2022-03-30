dc2b11ac5f10fd8586a8260331242c80c0435e48
Joseph Brown <zdragoonmaster@aol.com>
2022-03-30 00:16:19 -0700

    new flask models

---
+++ flask/fundamentals/Playground/server.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+from flask import Flask
+app == Flask(__name__)
+
+@app.route('/play')
+def levelOne():
+    return
+
+if __name__ == "__main__":
+    app.run(debug=True)
