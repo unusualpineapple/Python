bf40c52f4ad6510df6d6a1e3db3a9b0c67721b29
Joseph Brown <zdragoonmaster@aol.com>
2022-03-30 22:05:54 -0700

    updated playground

--- flask/fundamentals/Playground/server.py
+++ flask/fundamentals/Playground/server.py
@@ -1,10 +1,18 @@
-from unicodedata import name
-from flask import Flask
-app == Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 @app.route('/play')
 def levelOne():
-    return
+    return render_template("index.html", num=3, color="blue")
+
+@app.route('/play/<int:num>')
+def levelTwo(num):
+    return render_template("index.html", num=num, color="blue")
+
+@app.route('play/<int:num>/<string:color>')
+def levelThree(num, color):
+    return render_template("index.html", num=num, color=color)
 
 if __name__ == "__main__":
     app.run(debug=True)
