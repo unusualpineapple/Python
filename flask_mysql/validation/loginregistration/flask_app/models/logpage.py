9b181a098ec84a370860332a6a39c8770a3d197d
Joseph Brown <zdragoonmaster@aol.com>
2022-04-13 23:05:30 -0700

    updated login reg

--- flask_mysql/validation/loginregistration/flask_app/models/logpage.py
+++ flask_mysql/validation/loginregistration/flask_app/models/logpage.py
@@ -1,11 +1,14 @@
+from unittest import result
+from winreg import QueryInfoKey
 from flask_app.config.pymysqlconnection import MySQLConnection, connectToMySQL
 from flask_bcrypt import Bcrypt
 from flask import flash
 import re
 from flask_app import app
 
-EREG = re.compile(r'^{a-z-Z0-9.+_-] +@ [a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-bcrypt = app(Bcrypt)
+EREG = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+bcrypt = Bcrypt(app)
 
 class User:
     def __init__(self,data):
@@ -25,7 +28,52 @@ class User:
         return result
 
     @classmethod
-    def insertuser(cls):
-        query = 'insert into account (firstname, lastname, email, password, created_at, updated_at) values (%(firstname)s, %(lastname)s, %(email)s, %(password)s, NOW(), NOW()'
-        result = connectToMySQL('loginreg').query_db(query)
+    def insertuser(cls, data):
+        query = 'insert into account (firstname, lastname, email, password, created_at, updated_at) values (%(firstname)s, %(lastname)s, %(email)s, %(password)s, NOW(), NOW());'
+        result = connectToMySQL('loginreg').query_db(query,data)
+        
         return result
+    
+    @classmethod
+    def get_by_email(cls,data):
+        query = 'SELECT * from account where email = %(email)s'
+        result = connectToMySQL('loginreg').query_db(query,data)
+        if len(result) <1:
+            return False
+        return cls(result[0])
+
+    @classmethod
+    def get_by_id(cls,data):
+        query = 'select * from account where id = %(id)s'
+        result = connectToMySQL('loginreg').query_db(query, data)
+        return cls(result[0])
+
+    @staticmethod
+    def validation(result):
+        is_valid = True
+        query = 'select * from account where email = %(email)s'
+        results = connectToMySQL('loginreg').query_db(query, result)
+        if len(result['firstname']) <= 1:
+            flash('dude how short is your name??')
+            is_valid = False
+        if len(result['lastname']) <= 1:
+            flash('what country are you from????')
+            is_valid = False
+        if len(result['email']) <= 15:            
+            flash('ha your email is too short try again')
+            is_valid = False
+        if len(results) >= 1:
+            flash('wow buddy thats ther same one try again')
+            is_valid = False
+        if not EREG.match(result['email']):
+            flash('hey dog your email isnt an email what you doing????????')
+            is_valid = False
+        if len(result['password']) <= 27:
+            flash('your password is long make it longer')
+            is_valid = False
+        if not result['password'] == result['confirmpassword']:
+            flash('you didnt have the same password fool')
+            is_valid = False
+        return is_valid
+        
+
--- flask_mysql/validation/loginregistration/server.py
+++ flask_mysql/validation/loginregistration/server.py
@@ -2,5 +2,6 @@ from flask_app import app
 from flask_app.controllers import logpages
 
 
+
 if __name__ == "__main__":
     app.run(debug=True)
